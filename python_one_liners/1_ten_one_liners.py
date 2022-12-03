e764d7565e198b0eb2f57ed06c031600ded88286
alex.jean.derae <alex.jean.derae@gmail.com>
2022-12-04 09:20:22 +1100

    rereading - improved comments

--- python_one_liners/1_ten_one_liners.py
+++ python_one_liners/1_ten_one_liners.py
@@ -1,6 +1,5 @@
 
 # 1 Swap two variables
-
 a = 4 
 b = 5
 print(a,b)
@@ -21,23 +20,29 @@ print(a,b,c)
 # >> 1 2 [3, 4, 5]
 
 # 4 Sum of even numbers in a list
+
 a = [1,2,3,4,5,6]
 s = sum([num for num in a if num%2 == 0]) # the one liner
 print(s)
 # >> 12
 
-# 5 Deleting Multiple Elements from a List with the del keyword
+# 5 Deleting Multiple Elements from a List with the del keyword and extended slices
 
 ## extended slices aa[start:end:step] and is all on the index, python does not look at the content here
 
 #### Deleting all the even numbers
+## Not a great title for the technique. "Even" and "numbers" are not garanteed in the code.
+## Deleting all numbers present by index +2 starting at index 1 is more correct but less catchy.
+## If the list started with a value of 0 instead of 1 the code below would need a small change.
+
 a = [1,2,3,4,5]
 del a[1::2] # the one liner
 print(a)
 # >>[1, 3, 5]
 # this one uses the "extended slices", jumping by 2 starting at index 1
 
-#### Deleting all the numbers at odd index
+#### Deleting all the odd numbers
+## see comments above for even numbers.
 a = [1,2,3,4,5]
 del a[::2] # the one liner, no need to write index 0 as starting
 print(a)
@@ -79,8 +84,9 @@ print(lst)
 # >> ['Hello Karl', 'Hello Abhay', 'Hello Zen']
 
 # 10 change the type of a whole list using map instead of iteration (aka Typecasting)
-
-##Example we have a few values we handpicked and they are currently strings and we want them float
 lst = list(map(float,['1','34','270']))
 print(lst)
 # >> [1.0, 34.0, 270.0]
+## In this example we have a few alphanumberical values that are currently *strings* and we want them becoming floats
+## works with int as well in both ways. The '' removed would make them int in the example below and you get floats.
+## replacing float by int in the one liner gets you 1,34,270 as integers instead of strings.
