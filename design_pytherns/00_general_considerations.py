ebf43336feac8f9caa9c5d01b3a081eb707edf51
alex.jean.derae <alex.jean.derae@gmail.com>
2022-12-05 05:58:56 +1100

    update

--- design_pytherns/00_general_considerations.py
+++ design_pytherns/00_general_considerations.py
@@ -2,5 +2,4 @@
 # Why COMPOSITION is better than INHERITANCE - detailed Python example
 # https://www.youtube.com/watch?v=0mcP8ZpUR38
 
-# Advanced python tutorials series
-# https://www.youtube.com/watch?v=KSiRzuSx120&list=PL7yh-TELLS1FuqLSjl5bgiQIEH25VEmIc
+
